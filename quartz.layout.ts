3c44a3550a420b023f0970536d8bc1c720d16a16
tlgn <sendmework@ya.ru>
2025-05-06 09:24:47 +0300

    Margin fix

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [
-    Component.Marquee(),
+    // Removing the Marquee component to get rid of the top text
   ],
   afterBody: [],
   footer: Component.Footer({
